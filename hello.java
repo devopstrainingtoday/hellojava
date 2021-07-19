e29d7870d5a31abd9dcf36a03651cadd63bf23b5
raop12044 <87634802+raop12044@users.noreply.github.com>
2021-07-19 17:16:39 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class Hello {
    /* This is my first java program.
     * This will print 'Hello World' as the output
     */
-
+   // author pavan
    public static void main(String []args) {
       System.out.println("Hello World"); // prints Hello World
    }
